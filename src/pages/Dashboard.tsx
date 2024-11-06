4028d6b9b45c9e219e0e66786986b34acf796075
ilkeratik <ilkeratik35@gmail.com>
2024-11-06 14:28:41 +0000

    center fixed create button

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -16,7 +16,7 @@ const Dashboard = () => {
             </Row>
             <Button
                 className="bg-success px-4 py-3"
-                style={{ fontWeight: 'bold', position: 'fixed', bottom: '20px', right: '50px', borderRadius: '1rem' }}
+                style={{ fontWeight: 'bold', position: 'fixed', bottom: '20px', right: '48%', borderRadius: '1rem' }}
                 onClick={() => {
                     setCurrentAction("create");
                     setShowModal(true);
